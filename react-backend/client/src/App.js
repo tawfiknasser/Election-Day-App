2a3b88180c9c378707d3148175ecb961f46321f8
tawfiknasser <tawfiknasser@gmail.com>
2018-09-09 17:34:42 +0300

    #2 setup react and express

---
+++ react-backend/client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Election-Day App</h1>
+        </header>
+
+        <h3>Hello World</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
