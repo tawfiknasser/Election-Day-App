ff883d106f8b7ad58675735d9ab4797c6dc417e7
tawfiknasser <tawfiknasser@gmail.com>
2018-09-10 12:36:23 +0300

    #12 fixed the project

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
 
 
-var app = express();
+const app = express();
 
 
 app.use(logger('dev'));
@@ -18,21 +18,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-
-// catch 404 and forward to error handler
-app.use((req, res, next)=> {
-  next(createError(404));
-});
-
 // error handlerfunction
 app.use((err, req, res, next)=> {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send("error");
 });
 
 module.exports = app;
