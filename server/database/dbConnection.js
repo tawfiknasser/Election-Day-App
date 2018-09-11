93d18fc68e897ef6bcc3b87ad3246636b127f3b0
Ryan2601 <39595382+Ryan2601@users.noreply.github.com>
2018-09-11 15:19:14 +0300

    Update dbConnection.js

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -6,8 +6,4 @@ let { ELECTION_DAY_DB_URL } = process.env;
 if (!ELECTION_DAY_DB_URL)
   throw new Error('Enviroment variable TRADING_DB_URL must be set');
 
-const options = {
-  connectionString: ELECTION_DAY_DB_URL
-};
-
-module.exports = new Pool(options);
+module.exports = new Pool({connectionString: ELECTION_DAY_DB_URL});
