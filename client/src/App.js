d5c0594e5c3bb77e91b3d3de8bc56975abfc5aa8
tawfiknasser <tawfiknasser@gmail.com>
2018-09-11 12:08:25 +0300

    #23 added headerbar with burger menu element

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@
 import React, { Component } from 'react';
 import SearchRectangle from './components/search_info_components/search';
 import InfoBar from './components/search_info_components/infoBar';
+import HeaderBar from './components/Header_Bar/header';
+
 class App extends Component {
 render(){
   return (
     <React.Fragment>
-    <SearchRectangle />
+    <HeaderBar />
+    <SearchRectangle/>
     <InfoBar />
     </React.Fragment>
 
--- client/src/components/search_info_components/infoBar.js
+++ client/src/components/search_info_components/infoBar.js
@@ -1,17 +1,14 @@
 import React from "react";
 import "./infoBar.css";
-export default class GridItem extends React.Component {
+export default class infoBar extends React.Component {
   state = {
     clicked: false
   };
 
   render() {
-    return(
+    return (
       <React.Fragment>
-
-      <div className="infoBar">
-        Box : 432     Voters: 252/320
-      </div>
+        <section className="infoBar"><span>Box : 432 Voters: 252/320   </span></section>
       </React.Fragment>
     );
   }
