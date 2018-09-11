c3ad60119c362fa0006c94144a12f9f85af11085
tawfiknasser <tawfiknasser@gmail.com>
2018-09-12 01:43:37 +0300

    #9 added model file for dummy data before finishing the queries

---
+++ server/model/voters.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    name: "tawfik",
+    id: "2345773432",
+    serial: "457"
+  },
+  {
+    name: "tawgs",
+    id: "275323432",
+    serial: "499"
+  },
+  {
+    name: "tafgk",
+    id: "234632",
+    serial: "999"
+  },
+  {
+    name: "tawfgf",
+    id: "234323432",
+    serial: "057"
+  },
+  {
+    name: "gfgfgfk",
+    id: "234323432",
+    serial: "857"
+  },
+  {
+    name: "tafgik",
+    id: "2343454532",
+    serial: "257"
+  }
+];
