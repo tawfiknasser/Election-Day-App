d0c87ae6e4e25197d629d7770fa056b3a60571f6
tawfiknasser <36166288+tawfiknasser@users.noreply.github.com>
2018-09-12 13:58:31 +0300

    Removed next argument

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 const voters = require('../model/voters');
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.send('helloww');
 });
-router.get('/voters', (req, res, next) => {
+router.get('/voters', (req, res) => {
   res.json(voters);
 });
 
