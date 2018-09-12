83c75deca3c46cca197e0c9fa41e730b76bf0923
Ray <ryan.natoor@gmail.com>
2018-09-12 14:52:56 +0300

    #31 first set of queries

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -1,13 +1,12 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 
-let { ELECTION_DAY_DB_URL } = process.env;
+const { ELECTION_DAY_DB_URL } = process.env;
 
-if (!ELECTION_DAY_DB_URL)
-  throw new Error('Enviroment variable TRADING_DB_URL must be set');
+if (!ELECTION_DAY_DB_URL) throw new Error('Enviroment variable ELECTION_DAY_DB_URL must be set');
 
 const options = {
-  connectionString: ELECTION_DAY_DB_URL
+  connectionString: ELECTION_DAY_DB_URL,
 };
 
 module.exports = new Pool(options);
