83b742496fd93c6f646815c69ce4f08c021b20b2
Ray <ryan.natoor@gmail.com>
2018-09-12 14:55:59 +0300

    #31 first set of queries

--- server/database/build.sql
+++ server/database/build.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
-DROP TABLE IF EXISTS voters,users,box,region;
-
 CREATE TABLE voters(
   id INT PRIMARY KEY,
-  full_name VARCHAR(60) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  first_name VARCHAR(20) NOT NULL,
+  middle_name VARCHAR(20) NOT NULL,
   box_number SMALLINT NOT NULL,
   address VARCHAR(100) NULL,
   serial_box_number INT NOT NULL,
@@ -15,17 +15,17 @@ CREATE TABLE voters(
 
 CREATE TABLE region(
   id SERIAL PRIMARY KEY,
-  name VARCHAR(50) UNIQUE NOT NULL
+  name VARCHAR(50)
 );
 
 CREATE TABLE users(
-  id SERIAL PRIMARY KEY,
+  id INT PRIMARY KEY,
   full_name VARCHAR(100) UNIQUE NOT NULL,
   user_name VARCHAR(20) UNIQUE NOT NULL,
   password VARCHAR(20) NOT NULL,
   telephone INT NOT NULL,
   email VARCHAR(50) NULL,
-  type VARCHAR(20) NOT NULL
+  type VARCHAR(20) NOT NULL CHECK (type = 'B' OR type = 'R' OR type = 'M')
 );
 
 
