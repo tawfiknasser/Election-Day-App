7cf23c4b689f0db919d974cc3d0b3eb44d81c156
tawfiknasser <tawfiknasser@gmail.com>
2018-09-12 16:38:29 +0300

    #43 router now serve the static client build file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 const voters = require('../model/voters');
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.send('helloww');
+  res.sendFile(path.join(__dirname, '..', '..', 'client/build/index.html'));
 });
 router.get('/voters', (req, res) => {
   res.json(voters);
