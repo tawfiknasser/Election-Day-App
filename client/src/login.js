33009ef8fe9bc94ff95eb158b2a103d879002cab
yoosefkh <37940433+yoosefkh@users.noreply.github.com>
2018-09-12 16:08:53 +0300

    removed extra ">" symbol

--- client/src/login.js
+++ client/src/login.js
@@ -6,8 +6,8 @@ const Login = ()=>{
     <React.Fragment>
     <img src="https://user-images.githubusercontent.com/36166288/45061767-924b3300-b0ae-11e8-93fc-8e935fec045a.png" class="mainIcon" alt="icon"/>
     <div>
-      <input class="username" placeholder="username"> />
-      <input class="password" placeholder="password"> />
+      <input class="username" placeholder="username" />
+      <input class="password" placeholder="password" />
     </div>
     <div>
   <button id="loginbutt" >
