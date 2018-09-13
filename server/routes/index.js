c674d72aa781a4ffe477efff32fa69b977ef1ea4
tawfiknasser <tawfiknasser@gmail.com>
2018-09-13 20:28:53 +0300

    #60 changed the router to serve the react client/build file

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,18 @@ const path = require('path');
 const router = express.Router();
 const voters = require('../model/voters');
 /* GET home page. */
-// router.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', '..', 'client/build/index.html'));
-// });
+
 router.get('/voters', (req, res) => {
   res.json(voters);
 });
 
+if (process.env.NODE_ENV === 'production') {
+  router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'client/build/index.html'));
+  });
+} else {
+  router.get('*', (req, res) => {
+    res.send('server is working (dev mode)');
+  });
+}
 module.exports = router;
