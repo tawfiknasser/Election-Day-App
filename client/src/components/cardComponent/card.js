f9b1a00977c125b54aeb913a5832ba1f3d477cc7
tawfiknasser <tawfiknasser@gmail.com>
2018-09-13 11:53:16 +0300

    #49 add pressing functionalty and change box color when clicking

--- client/src/components/cardComponent/card.js
+++ client/src/components/cardComponent/card.js
@@ -3,13 +3,26 @@ import "./card.css";
 
 export default class Card extends React.Component {
   state = {
-    clicked: false // when clicked it should change color
+    clicked: false, // when clicked it should change color
+    classIs: "card cardUnclicked"
   };
-
+  colorClick = () => {
+    if (this.state.clicked) {
+      this.setState({
+        clicked: false,
+        classIs: "card cardUnclicked"
+      });
+    } else {
+      this.setState({
+        clicked: true,
+        classIs: "card cardClicked"
+      });
+    }
+  };
   render() {
     return (
       <React.Fragment>
-        <div className="card">
+        <div className={this.state.classIs} onClick={this.colorClick}>
           {this.props.name}
           {this.props.id}
         </div>
