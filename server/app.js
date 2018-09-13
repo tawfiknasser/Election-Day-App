c7f681ec487815884e54a0b87c785b5a4022b230
tawfiknasser <36166288+tawfiknasser@users.noreply.github.com>
2018-09-13 10:30:12 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,8 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '..', 'client/build')));
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'client/build')));
+}
 app.use('/', indexRouter);
 
 // error handlerfunction
