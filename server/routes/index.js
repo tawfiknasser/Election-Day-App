020ae5b1f7a207076fd1afd57804bf5351d063aa
yoosefkh <yoosef.khirat@gmail.com>
2018-09-16 10:47:43 +0300

    #5 added login handler made it functional sending post request to the server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const voters = require('../model/voters');
+const voters = require("../model/voters");
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.send('helloww');
+router.get("/", (req, res) => {
+  res.send("helloww");
 });
-router.get('/voters', (req, res) => {
+router.get("/voters", (req, res) => {
   res.json(voters);
+});
+router.post("/login", (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
