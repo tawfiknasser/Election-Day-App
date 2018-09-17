6028f24b130cedf91c6885efa6464916783e6450
tawfiknasser <tawfiknasser@gmail.com>
2018-09-17 03:47:57 +0300

    #65 refactored car component and added fetch to connect to the router

--- client/src/components/cardComponent/card.js
+++ client/src/components/cardComponent/card.js
@@ -8,22 +8,21 @@ export default class Card extends React.Component {
   };
   colorClick = () => {
     // confirm message first
+    let confirmValue = window.confirm(
+      `${this.state.clicked ? "Unmark" : "Mark"} ${this.props.name} ?`
+    );
+    if (!confirmValue) return;
+    fetch(`/?idVoter=${this.props.id}&status=${!this.state.clicked}`)
+      .then(
+        this.setState({
+          clicked: !this.state.clicked,
+          classIs: this.state.clicked
+            ? "card cardUnclicked"
+            : "card cardClicked"
+        })
+      )
 
-    if (this.state.clicked) {
-      let confirmValue = window.confirm(`Unmark ${this.props.name}  ?`);
-      if (!confirmValue) return;
-      this.setState({
-        clicked: false,
-        classIs: "card cardUnclicked"
-      });
-    } else {
-      let confirmValue = window.confirm(`Mark ${this.props.name}  ?`);
-      if (!confirmValue) return;
-      this.setState({
-        clicked: true,
-        classIs: "card cardClicked"
-      });
-    }
+      .catch(error => console.log(error));
   };
   render() {
     return (
--- server/database/queries/updateVoterStatus.js
+++ server/database/queries/updateVoterStatus.js
@@ -1,5 +1,5 @@
 const dbConnection = require('../dbConnection');
-
+// here it set the status as what is sent to her from the frontend
 const updateVoterStatus = (idOfVoter, status, cb) => {
   const queryString = 'UPDATE voters SET status = $1 WHERE id = $2';
   dbConnection.query(queryString, [status, idOfVoter], cb);
