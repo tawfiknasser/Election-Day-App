966168e25d16ca5a8308c4af44013c27623f549e
yoosefkh <yoosef.khirat@gmail.com>
2018-09-19 16:45:17 +0300

    resolve QA note

--- Utils/addUserHelper.js
+++ Utils/addUserHelper.js
@@ -4,18 +4,16 @@ const { hashPassword } = require('../server/helpers/hashHelper');
 const usernmae = process.argv[2];
 const password = process.argv[3];
 const id = process.argv[4];
-hashPassword(password, (error, hashedPassword) => {
-  if (error) {
-    console.log('error', error);
+hashPassword(password, (hasingError, hashedPassword) => {
+  if (hasingError) {
+    throw hasingError; // only runs in terminal
   } else {
     db_Connection.query(
       'insert into users (id,full_name,user_name,password,telephone,email,type) values ($1,$2,$3,$4,$5,$6,$7)',
       [id, usernmae, usernmae, hashedPassword, 242345, 'erge@j.com', 'B'],
-      (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('success user added', result);
+      (dbError) => {
+        if (dbError) {
+          throw dbError; // only runs in terminal
         }
       },
     );
