db9fdc50f5ecb71b0357eddacf571a910e7cd2e7
yoosefkh <yoosef.khirat@gmail.com>
2018-09-19 22:06:53 +0300

    fixing typo #82

--- Utils/addUserHelper.js
+++ Utils/addUserHelper.js
@@ -5,8 +5,8 @@ const usernmae = process.argv[2];
 const password = process.argv[3];
 const id = process.argv[4];
 hashPassword(password, (hasingError, hashedPassword) => {
-  if (hasingError) {
-    throw hasingError; // only runs in terminal
+  if (hashingError) {
+    throw hashingError; // only runs in terminal
   } else {
     db_Connection.query(
       'insert into users (id,full_name,user_name,password,telephone,email,type) values ($1,$2,$3,$4,$5,$6,$7)',
